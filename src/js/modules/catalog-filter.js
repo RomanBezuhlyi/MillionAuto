2a1b0462057e5fd93eb1563ca9c71d097e32ba2f
Роман Безуглий <romanbezuhlyi@MacBook-Pro-14.local>
2025-02-10 23:48:56 +0200

    added adaptive to catalog

--- src/js/modules/catalog-filter.js
+++ src/js/modules/catalog-filter.js
@@ -53,3 +53,30 @@ document.addEventListener('DOMContentLoaded', function () {
 		checkInputs() // перевіряємо кнопку та видимість кнопки "Ресет"
 	})
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+	const filterWrap = document.querySelector('.catalog__filter')
+	const filterToggleBtn = document.querySelector('.catalog__sort-btn--filter')
+	const filterCloseBtn = document.querySelector('.catalog__filter-wrap button')
+	const body = document.body
+
+	function toggleFilter() {
+		if (!filterWrap.classList.contains('show')) {
+			// Зберігаємо поточне положення прокрутки
+			body.dataset.scrollY = window.scrollY
+			filterWrap.style.top = `${window.scrollY}px`
+			body.style.top = `-${window.scrollY}px`
+			body.classList.add('filter-open')
+		} else {
+			// Відновлюємо скрол після закриття
+			body.classList.remove('filter-open')
+			window.scrollTo(0, parseInt(body.dataset.scrollY || '0'))
+			body.style.top = ''
+		}
+
+		filterWrap.classList.toggle('show')
+	}
+
+	filterToggleBtn.addEventListener('click', toggleFilter)
+	filterCloseBtn.addEventListener('click', toggleFilter)
+})
