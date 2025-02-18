05d16acedb6f7ac49464e91c774e1e832e502a2a
Роман Безуглий <romanbezuhlyi@MacBook-Pro-14.local>
2025-02-18 23:55:45 +0200

    added some pages

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import './modules/animation'
 import './modules/application'
+import './modules/car-preview'
 import './modules/catalog-filter'
 import './modules/dropdown'
 import './modules/faq'
---
+++ src/js/modules/car-preview.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const carItems = document.querySelectorAll('.car__item')
+	const carPreviewImg = document.querySelector('.car__preview-img')
+	let activeIndex = 0
+
+	function updateActiveItem() {
+		carItems.forEach(item => item.classList.remove('active'))
+		carItems[activeIndex].classList.add('active')
+		carPreviewImg.src = carItems[activeIndex].querySelector('img').src
+
+		const zoomButton = document.querySelector('.car__zoom')
+		zoomButton.setAttribute(
+			'href',
+			carItems[activeIndex].querySelector('img').src
+		)
+	}
+
+	carItems.forEach((item, index) => {
+		item.addEventListener('click', function () {
+			activeIndex = index
+			updateActiveItem()
+		})
+	})
+
+	const nextButton = document.querySelector('.car__button-next')
+	nextButton.addEventListener('click', function () {
+		activeIndex = (activeIndex + 1) % carItems.length
+		updateActiveItem()
+	})
+
+	const prevButton = document.querySelector('.car__button-prev')
+	prevButton.addEventListener('click', function () {
+		activeIndex = (activeIndex - 1 + carItems.length) % carItems.length
+		updateActiveItem()
+	})
+
+	updateActiveItem()
+})
+const likeButton = document.getElementById('likeButton')
+
+likeButton.addEventListener('click', function () {
+	this.classList.toggle('like') // Додає або знімає клас "like"
+})
